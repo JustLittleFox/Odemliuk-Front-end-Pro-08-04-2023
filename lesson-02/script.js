0ef2ace632fa530df4ca6e2c5bf947be62fab2a9
JustLittleFox <13fox.fox.fox.13@gmail.com>
2023-08-04 17:17:43 +0200

    done hw lesson-2

---
+++ lesson-02/script.js
@@ -0,0 +1,19 @@
+//////// First task
+const userInput1 = prompt('First value:');
+const userInput2 = prompt('Second value:');
+const userInput3 = prompt('Third value:');
+
+const result = userInput2 + userInput3 + userInput1;
+
+alert('Result:  ' + result + '');
+console.log(`${userInput2 + userInput3 + userInput1}`);
+
+
+/////// Second task
+const userNumbers = prompt('Enter a five - digit number:');
+const number = userNumbers;
+
+const result2 = userNumbers.split('').join(' ');
+
+alert(+ number);
+console.log(result2);
