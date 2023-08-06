398c1cba1010b70f6cdf33fb445d22dc6fc193fc
JustLittleFox <13fox.fox.fox.13@gmail.com>
2023-08-06 15:25:00 +0200

    fixed hw lesson-1

--- lesson-01/script.js
+++ lesson-01/script.js
@@ -1,4 +1,17 @@
-var name = prompt('What is your name?');
-var feel = prompt('Hello, ' + name + '! How are you?');
-alert('I am glad that you ' + feel + '');
+const name = prompt('What is your name?', 'Please, enter your name here');
+if (name === null) {
+    alert('Value is not provided');
+} else if (name === ('')) {
+    alert('Name is not provided');
+} else {
+    const feel = prompt('Hello, ' + name + '! How are you?', 'Please, enter your feeling here');
+    if (feel == null) {
+        alert('Value is not provided');
+    } else if (feel == ('')) {
+        alert('Feeling is not provided');
+    } else {
+        alert('I am glad that you ' + feel + '');
+        console.log(name, feel);
+    }
+}
 
