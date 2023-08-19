4be6291e70a7a08ac6c131a2a2e92ce07943d11b
JustLittleFox <13fox.fox.fox.13@gmail.com>
2023-08-19 18:43:14 +0200

    fixed problems with homework-05

--- homework-05/script.js
+++ homework-05/script.js
@@ -20,22 +20,22 @@ if (age === null) {
 
             let message = `You are ${2023 - age} years old \nYou live in ${city}`;
 
-            if (city === `Kyiv`) {
+            if (city === 'Kyiv', 'kyiv', 'Київ', 'київ') {
                 message += `, the capital of Ukraine`;
-            } else if (city === `Washington`) {
+            } else if (city === 'Washington', 'washington', 'Вашингтон', 'вашингтон') {
                 message += `, the capital of USA`;
-            } else if (city === `London`) {
+            } else if (city === 'London', 'london', 'Лондон', 'лондон') {
                 message += `, the capital of Great Britan`;
-            } else if (city === `Warsaw`) {
+            } else if (city === 'Warsaw', 'warsaw', 'Варшава', 'варшава') {
                 message += `, the capital of Poland`;
             }
 
-            if (sport === `hockey`) {
+            if (sport === 'hockey', 'Hockey', 'хоккей', 'Хоккей') {
                 message += `\nCool, you whant to be like Alexandr Ovechkin?`;
-            } else if (sport === `basketball`) {
+            } else if (sport === 'basketball', 'Basketball', 'баскетбол', 'Баскетбол') {
                 message += `
                 Cool, you whant to be like Michael Jordan?`;
-            } else if (sport === `football`) {
+            } else if (sport === 'football', 'Football', 'футбол', 'Футбол') {
                 message += `\nCool, you whant to be like Cristiano Ronaldo?`;
             } else if (sport === sport) {
                 message += `\nCool, you whant to be ${sport} champion?`
