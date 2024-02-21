e7310b08ed2c2cda3f8663b51a90a77d13d59a79
JustLittleFox <13fox.fox.fox.13@gmail.com>
2024-02-21 15:19:42 +0100

    used template string

--- 70-AjaxWeather/script.js
+++ 70-AjaxWeather/script.js
@@ -1,4 +1,4 @@
-const city = "WROCLAW";
+const city = "Wroclaw";
 const apiKey = "5d066958a60d315387d9492393935c19";
 
 const xhr = new XMLHttpRequest();
@@ -9,14 +9,14 @@ xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
         const data = JSON.parse(xhr.responseText);
         let weather = "";
-        weather += "city: " + data.name + "<br>";
-        weather += "temp: " + data.main.temp + "<br>";
-        weather += "pressure: " + data.main.pressure + "<br>";
-        weather += "description: " + data.weather[0].description + "<br>";
-        weather += "humidity: " + data.main.humidity + "<br>";
-        weather += "wind speed: " + data.wind.speed + "<br>";
-        weather += "wind deg: " + data.wind.deg + "<br>";
-        weather += "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>";
+        weather += `city: ${data.name}<br>`;
+        weather += `temp: ${data.main.temp}<br>`;
+        weather += `pressure: ${data.main.pressure}<br>`;
+        weather += `description: ${data.weather[0].description}<br>`;
+        weather += `humidity: ${data.main.humidity}<br>`;
+        weather += `wind speed: ${data.wind.speed}<br>`;
+        weather += `wind deg: ${data.wind.deg}<br>`;
+        weather += `<img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'>`;
         document.getElementById("weather").innerHTML = weather;
     }
 };
